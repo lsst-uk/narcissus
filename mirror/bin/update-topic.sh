2335b6ad517b2c6e02014e778c0075056e96e19d
Ubuntu <G Francis>
2019-12-05 11:19:32 +0000

    add topic change script

---
+++ mirror/bin/update-topic.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Update our topic names.
+topiclist=(
+    "ztf_$(date -d '2 days ago' +%Y%m%d)_programid1"
+    "ztf_$(date -d yesterday +%Y%m%d)_programid1"
+    "ztf_$(date -d today +%Y%m%d)_programid1"
+    )
+
+# https://stackoverflow.com/a/11360591
+topicstr="${topiclist[@]}"
+topicstr=${topicstr// /,}
+
+
+#
+# Stop the MirrorMaker process.
+sudo docker-compose \
+    --file "${HOME}/mirror-compose.yml" \
+    down
+
+#
+# Update the topic names.
+sed -E -i '
+    /whitelist/,/]/ {
+        /(whitelist|])/ !{
+            s/"[^"]*"/"'${topicstr}'"/
+            }
+        }
+    ' mirror-compose.yml
+
+#
+# Start the MirrorMaker process.
+sudo docker-compose \
+    --file "${HOME}/mirror-compose.yml" \
+    up --detach \
+        mirrormaker
